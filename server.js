006add6c8c5c73ac48c4c1935addb9121f19981e
RealLouna <lpds2011@outlook.fr>
2024-11-24 00:22:09 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Héberger les fichiers statiques
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('Nouvel utilisateur connecté');
+
+  // Recevoir et émettre un message
+  socket.on('sendMessage', (message) => {
+    io.emit('receiveMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Utilisateur déconnecté');
+  });
+});
+
+server.listen(4000, () => {
+  console.log('Serveur en cours d\'exécution sur le port 4000');
+});
