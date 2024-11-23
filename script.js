345ca9608a8e88c774fd51bafcfc55510f4cbbf9
RealLouna <lpds2011@outlook.fr>
2024-11-24 00:23:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,16 +12,24 @@ function textToMorse(text) {
   return text.toUpperCase().split('').map(char => morseDict[char] || char).join(' ');
 }
 
+// Initialiser Socket.IO
+const socket = io('http://localhost:4000');
+
 // Fonction pour envoyer un message
 function sendMessage() {
+  const username = document.getElementById('username').value;
   const messageInput = document.getElementById('message-input');
   const messageText = messageInput.value;
   const morseMessage = textToMorse(messageText);
 
-  const messagesDiv = document.getElementById('messages');
-  const newMessageDiv = document.createElement('div');
-  newMessageDiv.innerHTML = `<p><strong>Texte:</strong> ${messageText}</p><p><strong>Morse:</strong> ${morseMessage}</p>`;
-  messagesDiv.appendChild(newMessageDiv);
-  
+  socket.emit('sendMessage', { username, text: messageText, morse: morseMessage });
   messageInput.value = '';
 }
+
+// Recevoir un message
+socket.on('receiveMessage', (message) => {
+  const messagesDiv = document.getElementById('messages');
+  const newMessageDiv = document.createElement('div');
+  newMessageDiv.innerHTML = `<p><strong>${message.username}</strong>: ${message.text} <br><strong>Morse:</strong> ${message.morse}</p>`;
+  messagesDiv.appendChild(newMessageDiv);
+});
