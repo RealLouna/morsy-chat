9b4c406102fc3d9b4910a77501733831f637192f
Sammy24547 <lpds2011@outlook.fr>
2024-11-24 00:25:55 +0100

    GetMessage

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 // Héberger les fichiers statiques
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
   console.log('Nouvel utilisateur connecté');
